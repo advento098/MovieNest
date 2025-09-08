56ef47f9052ebdee1fffb0e015ca346d3201f71a
Pons Anthony Advento <ponsadvento08@gmail.com>
2025-09-08 21:17:46 +0800

    Testing json deserialization

--- backend/MovieNestBackend/Controllers/MoviesController.cs
+++ backend/MovieNestBackend/Controllers/MoviesController.cs
@@ -2,11 +2,14 @@ using Microsoft.AspNetCore.Http;
 using System.Net.Http;
 using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
+using MovieNestBackend;
 
 namespace MyApp.Namespace
 {
     [Route("api/[controller]")]
     [ApiController]
+
+
     public class MoviesController : ControllerBase
     {
         private readonly HttpClient _httpClient;
@@ -17,10 +20,13 @@ namespace MyApp.Namespace
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetPopularMovies()
+        public async Task<IActionResult> GetPopularMovies([FromQuery] string query)
         {
+            if (string.IsNullOrWhiteSpace(query))
+                return BadRequest("Add something on the query");
+
             const string apiKey = "110ecaa3";
-            const string url = $"http://www.omdbapi.com/?s=spiderman&apikey={apiKey}";
+            string url = $"http://www.omdbapi.com/?s={Uri.EscapeDataString(query)}&apikey={apiKey}";
 
             var response = await _httpClient.GetAsync(url);
             if (!response.IsSuccessStatusCode)
@@ -28,7 +34,10 @@ namespace MyApp.Namespace
 
             var content = await response.Content.ReadAsStringAsync();
             var json = JsonDocument.Parse(content);
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            var json2 = JsonSerializer.Deserialize<ResponseTemplate>(content, options);
 
+            Console.WriteLine($"json2 is: {json2}");
             return Ok(json);
         }
     }
